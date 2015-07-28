dd18320d60e5c10e83e8e8a2d6014aa9c471f1f6
MichaelC <m@michaelcullum.com>
2015-07-28 11:40:59 +0100

    Update dependencies to add serializer

--- src/Eyewitness/registerProviders.php
+++ src/Eyewitness/registerProviders.php
@@ -5,6 +5,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Silex\Provider\ValidatorServiceProvider;
 
 $app->register(new ValidatorServiceProvider());
+$app->register(new Silex\Provider\SerializerServiceProvider());
 $app->register(new DoctrineServiceProvider(), array(
     'dbs.options' => array(array(
         'driver'    => 'pdo_mysql',
