b73cb8c68bc1fc5f9518f0c2a1f46eac0a36dba2
MichaelC <m@michaelcullum.com>
2015-07-28 12:16:49 +0100

    Parse JSON requests

--- web/index.php
+++ web/index.php
@@ -4,6 +4,8 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Silex\Application;
 use Eyewitness\Router;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 include __DIR__.'/config.php';
 
@@ -14,6 +16,13 @@ $router = new Router($app);
 
 // Register service providers
 include __DIR__.'/../src/EyeWitness/registerProviders.php';
+
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
 
 $app->mount('/', $router->setBasicRoutes());
 
