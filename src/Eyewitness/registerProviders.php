297407d57e47aee87e46a65fc237a23867e43b13
MichaelC <m@michaelcullum.com>
2015-07-31 00:13:56 +0100

    Commit missing service provider

--- src/Eyewitness/registerProviders.php
+++ src/Eyewitness/registerProviders.php
@@ -6,6 +6,7 @@ use Silex\Provider\ValidatorServiceProvider;
 
 $app->register(new ValidatorServiceProvider());
 $app->register(new Silex\Provider\SerializerServiceProvider());
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
 $app->register(new DoctrineServiceProvider(), array(
 	'dbs.options' => array(array(
 		'driver'    => 'pdo_mysql',
